f9bfa4fb3e32b4d1717e3834302ff3c53751a4ac
Anna_Matveeva <75207035+AnikaBaykus@users.noreply.github.com>
2021-10-15 16:16:23 +0300

    add step 4

--- contacts.js
+++ contacts.js
@@ -1,6 +1,8 @@
+const { Command } = require("commander");
+const program = new Command();
+
 const fs = require("fs").promises;
 const path = require("path");
-
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 module.exports = {
@@ -14,3 +16,39 @@ const {
   addContact,
   removeContact,
 } = require("./operations");
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const invokeAction = ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      listContacts();
+      break;
+
+    case "get":
+      getContactById(id);
+      break;
+
+    case "add":
+      addContact(name, email, phone);
+      break;
+
+    case "remove":
+      removeContact();
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
